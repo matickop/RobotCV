8af25106904f8d2d6b6eaac412f0505f184afff2
Matic Koprivnikar <144683355+matickop@users.noreply.github.com>
2025-10-02 13:59:40 +0100

    Update

--- GUI.py
+++ GUI.py
@@ -2,17 +2,11 @@ import time
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import FreeSimpleGUI as sg
-from robot_module import MyRobot
-from kamera_module import MyCamera
 import test_main
 import threading
 
 stop_requested = False
 running_thread = None
-
-cam = MyCamera()
-
-robot = MyRobot("192.168.3.102")  
 pobrani_koti = [None]*8  # 4 koti prve palete + 4 druge
 
 layout = [
@@ -39,105 +33,105 @@ while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED:
-        robot.disconnect()
+        test_main.robot.disconnect()
         break
     
     if event ==  "Home":
-        robot.homing()
+        test_main.robot.homing()
 
     if event == "Activate gripper":
-        robot.gripper.activate()
+        test_main.robot.gripper.activate()
 
     if event == "Reconnect":
-        robot.reconnect()
+        test_main.robot.reconnect()
 
     if event == "STOP":
         test_main.stop_requested = True
 
     if event == "FreeDrive":
-        if not robot.freedrive_active:
-            robot.activate_freedrive()
+        if not test_main.robot.freedrive_active:
+            test_main.robot.activate_freedrive()
             window[event].update(text="FreeDrive ON", button_color=('white','red'))
         else:
-            robot.deactivate_freedrive()
+            test_main.robot.deactivate_freedrive()
             window[event].update(text="FreeDrive", button_color=('white','green'))
 
     elif event.startswith("Kot"):
         idx = int(event.split(" ")[1])-1
-        tcp = robot.get_actual_tcp_pose()
-        robot.pobrani_koti[idx] = tcp.copy()
-        np.save("koti.npy", np.array(robot.pobrani_koti, dtype=object))
+        tcp = test_main.robot.get_actual_tcp_pose()
+        test_main.robot.pobrani_koti[idx] = tcp.copy()
+        np.save("koti.npy", np.array(test_main.robot.pobrani_koti, dtype=object))
         # osveži prikaz
         prikaz = ""
-        for i, k in enumerate(robot.pobrani_koti):
+        for i, k in enumerate(test_main.robot.pobrani_koti):
             if k is not None:
                 prikaz += f"Kot {i+1}: {k}\n"
         window["-KOTI-"].update(prikaz)
 
     elif event == "Generiraj mrežo palete 1":
-        if any(k is None for k in robot.pobrani_koti[:4]):
+        if any(k is None for k in test_main.robot.pobrani_koti[:4]):
             sg.popup("Najprej poberi vse kote prve palete!")
             continue
         # generating
-        (robot.paleta1_safe,
-        robot.paleta1_work,
-        robot.paleta1_kam,
-        robot.paleta1_safe_joint,
-        robot.paleta1_work_joint,
-        robot.paleta1_kam_joint) = robot.pripravi_in_shrani_paleto("paleta1", robot.pobrani_koti[:4], "1")
+        (test_main.robot.paleta1_safe,
+        test_main.robot.paleta1_work,
+        test_main.robot.paleta1_kam,
+        test_main.robot.paleta1_safe_joint,
+        test_main.robot.paleta1_work_joint,
+        test_main.robot.paleta1_kam_joint) = test_main.robot.pripravi_in_shrani_paleto("paleta1", test_main.robot.pobrani_koti[:4], "1")
         sg.popup("Mreža palete 1 generirana!")
 
     elif event == "Generiraj mrežo palete 2":
-        if any(k is None for k in robot.pobrani_koti[4:]):
+        if any(k is None for k in test_main.robot.pobrani_koti[4:]):
             sg.popup("Najprej poberi vse kote druge palete!")
             continue
 
-        (robot.paleta2_safe,
-        robot.paleta2_work,
-        robot.paleta2_kam,
-        robot.paleta2_safe_joint,
-        robot.paleta2_work_joint,
-        robot.paleta2_kam_joint) = robot.pripravi_in_shrani_paleto("paleta2", robot.pobrani_koti[4:], "2")
+        (test_main.robot.paleta2_safe,
+        test_main.robot.paleta2_work,
+        test_main.robot.paleta2_kam,
+        test_main.robot.paleta2_safe_joint,
+        test_main.robot.paleta2_work_joint,
+        test_main.robot.paleta2_kam_joint) = test_main.robot.pripravi_in_shrani_paleto("paleta2", test_main.robot.pobrani_koti[4:], "2")
         sg.popup("Mreža palete 2 generirana!")
 
     elif event == "Naloži palete 1":
-        robot._load_paleta(1)
+        test_main.robot._load_paleta(1)
         sg.popup("Paleta 1 naložena")
 
     elif event == "Naloži palete 2":
-        robot._load_paleta(2)
+        test_main.robot._load_paleta(2)
         sg.popup("Paleta 2 naložena!")
 
     if event == "Shuffle kosckov":
         threading.Thread(target=test_main.shuffling_kosckov, daemon=True).start()
 
     if event == "Zajem slike":
-        cam.capture_image()
+        test_main.cam.capture_image()
 
     if event == "Disconnect camera":
-        cam.release()
+        test_main.cam.release()
         print("kamera disconnectana")
 
     if event == "Connect camera":
-        cam.connect()
+        test_main.cam.connect()
         print("kamera connectana")
 
     if event == "Template matching":
-        cam.template_match(cam.template_path, show=False)
+        test_main.cam.template_match(test_main.cam.template_path, show=False)
         print("template matcham")
 
     if event == "Pobiranje koščkov s kamero":
         threading.Thread(target=test_main.pobiranje_s_kamero, daemon=True).start()
         
     if event == "Generacija random mreže":
-        robot.generiranje_nakljucne_mreze(robot.paleta2.shape[0], robot.paleta2.shape[1], robot.paleta2)
-        print(robot.generiranje_nakljucne_mreze(robot.paleta2.shape[0], robot.paleta2.shape[1], robot.paleta2))
+        test_main.robot.generiranje_nakljucne_mreze(test_main.robot.paleta2.shape[0], test_main.robot.paleta2.shape[1], test_main.robot.paleta2)
+        print(test_main.robot.generiranje_nakljucne_mreze(test_main.robot.paleta2.shape[0], test_main.robot.paleta2.shape[1], test_main.robot.paleta2))
 
     if event == "Izhodiščna točka palete 1":
-        robot.move_to_position(robot.paleta1_safe_joint[0,0])
+        test_main.robot.move_to_position(test_main.robot.paleta1_safe_joint[0,0])
 
     if event == "Izhodiščna točka palete 2":
-        robot.move_to_position(robot.paleta2_safe_joint[0,0])
+        test_main.robot.move_to_position(test_main.robot.paleta2_safe_joint[0,0])
 
     if event == "Zajem celotne slike":
         test_main.zajem_celotne_slike()
